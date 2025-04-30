918500de2dcbf3d52ef827610248b0daff31129a
Icíar Lloréns Jover <iciar.llorensjover@meteoswiss.ch>
2025-04-30 17:43:51 +0200

    simplify init signature

--- obsweatherscale/models/gp_model.py
+++ obsweatherscale/models/gp_model.py
@@ -2,7 +2,9 @@ from typing import Any
 
 import torch
 from gpytorch.distributions import MultivariateNormal
+from gpytorch.kernels import Kernel
 from gpytorch.likelihoods import _GaussianLikelihoodBase
+from gpytorch.means import Mean
 from gpytorch.models import ExactGP
 
 
@@ -14,7 +16,8 @@ class GPModel(ExactGP):
         train_x: torch.Tensor,
         train_y: torch.Tensor,
         likelihood: _GaussianLikelihoodBase,
-        modules: dict,
+        mean_module: Mean,
+        covar_module: Kernel,
     ) -> None:
         """Initialize the GPModel.
 
@@ -26,14 +29,16 @@ class GPModel(ExactGP):
             The training output data.
         likelihood : _GaussianLikelihoodBase
             The likelihood function for the model.
-        modules : dict
-            A dictionary containing the mean and covariance modules.
+        mean_module : Mean
+            The mean module
+        covar_module : Kernel
+            The covariance module (kernel)
         """
         super().__init__(train_x, train_y, likelihood)
-        self.mean_module = modules["mean_module"]
-        self.covar_module = modules["covar_module"]
+        self.mean_module = mean_module
+        self.covar_module = covar_module
 
     def forward(self, x: torch.Tensor, **kwargs: Any) -> MultivariateNormal:
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return MultivariateNormal(mean_x, covar_x)
+        return MultivariateNormal(mean_x, covar_x)  # type: ignore
