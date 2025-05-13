0d5eb68de58e787343c142e379c358fe6ee6ca64
Icíar Lloréns Jover <iciarllorens@gmail.com>
2025-05-13 13:07:25 +0200

    refactor examples

--- examples/example_inference_script.py
+++ examples/example_inference_script.py
@@ -15,43 +15,65 @@ from obsweatherscale.transformations import QuantileFittedTransformer
 from obsweatherscale.transformations.standardizer import Standardizer
 
 
-def main():
-    #### Data ####
-    def true_signal(x, y, t):
-        return (
-            torch.sin(math.pi * x) *
-            torch.cos(math.pi * y) *
-            torch.sin(2 * math.pi * t / t.shape[0])
-        )
+def true_signal(x, y, t):
+    return (
+        torch.sin(math.pi * x) *
+        torch.cos(math.pi * y) *
+        torch.sin(2 * math.pi * t / t.shape[0])
+    )
 
-    n_stations, n_x, n_y, n_times, noise_var = 100, 30, 20, 10, 0.1
+def generate_toy_grid_data(n_x, n_y, n_times, noise_var):
     n_points = n_x * n_y
     t = torch.linspace(0, n_times - 1, n_times)
 
-    # Target data: the desired grid
     t_grid, x_grid, y_grid = torch.meshgrid(
         t, torch.linspace(0, 1, n_x), torch.linspace(0, 1, n_y), indexing='ij'
     )
 
-    target_x = torch.stack([t_grid, x_grid, y_grid], dim=-1)
-    target_y = true_signal(x_grid, y_grid, t_grid) + \
+    ds_x = torch.stack([t_grid, x_grid, y_grid], dim=-1)
+    ds_y = true_signal(x_grid, y_grid, t_grid) + \
         math.sqrt(noise_var) * torch.randn_like(x_grid)
 
-    target_x = target_x.reshape(n_times, n_points, 3) # [n_times, n_points, 3]
-    target_y = target_y.reshape(n_times, n_points)    # [n_times, n_points]
+    ds_x = ds_x.reshape(n_times, n_points, 3) # [n_times, n_points, 3]
+    ds_y = ds_y.reshape(n_times, n_points)    # [n_times, n_points]
 
-    # Context data: the stations (used for training)
+    return ds_x, ds_y
+
+
+def generate_toy_point_data(n_stations, n_times, noise_var):
+    t = torch.linspace(0, n_times - 1, n_times)
+
     x_coords, y_coords = torch.rand(1, n_stations), torch.rand(1, n_stations)
     x_coords = x_coords.expand(n_times, -1)    # [n_times, n_stations]
     y_coords = y_coords.expand(n_times, -1)    # [n_times, n_stations]
     t = t.unsqueeze(-1).expand(-1, n_stations) # [n_times, n_stations]
 
-    context_x = torch.stack([x_coords, y_coords, t], dim=-1)
-    context_y = true_signal(x_coords, y_coords, t) + \
+    ds_x = torch.stack([x_coords, y_coords, t], dim=-1)
+    ds_y = true_signal(x_coords, y_coords, t) + \
         math.sqrt(noise_var) * torch.randn_like(x_coords)
 
+    return ds_x, ds_y
+
+
+def main():
+    #### Data ####
+    n_stations, n_x, n_y, n_times, noise_var = 100, 30, 20, 10, 0.1
+
+    # Target data: the desired grid
+    target_x, target_y = generate_toy_grid_data(n_x, n_y, n_times, noise_var)
+
+    # Context data: the stations (used for training)
+    context_x, context_y = generate_toy_point_data(
+        n_stations, n_times, noise_var
+    )
+
     # Load artifacts and normalize
-    # Note: here we instantiate them untrained, but they should be loaded
+    # Note: here we instantiate them untrained but they should be loaded, e.g.:
+    # with open("standardizer.pkl", 'rb') as standardizer_file:
+    #     standardizer = pickle.load(standardizer_file)
+    # with open("y_transformer.pkl", 'rb') as y_transformer_file:
+    #     y_transformer = pickle.load(y_transformer_file)
+
     standardizer = Standardizer(context_x)
     y_transformer = QuantileFittedTransformer()
 
@@ -63,12 +85,12 @@ def main():
     # Initialize device
     if torch.cuda.is_available():
         torch.cuda.init()
-        device = torch.device("cuda") 
+        device = torch.device("cuda")
     else:
         device = torch.device("cpu")
 
     # Initialize likelihood
-    noise_model = TransformedFixedGaussianNoise(y_transformer, obs_noise_var=1)
+    noise_model = TransformedFixedGaussianNoise(y_transformer, noise_var)
     likelihood = TransformedGaussianLikelihood(noise_model)
 
     # Initialize model
